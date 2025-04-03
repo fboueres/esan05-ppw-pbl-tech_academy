455576669aafc9e63596b6f270d05c83cfdd5b1b
Fernando Boueres <boueres.fernando@gmail.com>
2025-04-03 12:53:54 +0000

    npx prettier --write resources/css&js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
